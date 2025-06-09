c8fdccacb2b35166c3925f06273f8e7eafc3e0da
Dannylo Torres <krdannylo.new@gmail.com>
2025-06-09 02:53:58 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 ({
   imports: [
@@ -14,11 +15,26 @@ import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  })
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     })
   )
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('brainAgriculture')
+    .setDescription('')
+    .setVersion('1.0')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, configSwagger)
+  SwaggerModule.setup('docs', app, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
